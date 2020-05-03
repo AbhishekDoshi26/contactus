2da16e42fd584827777ca3353f4ed413f5a9fccd
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-03 15:24:58 +0530

    v1.1.4

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
