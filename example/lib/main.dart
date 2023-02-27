10a4447f6940a636b7cc8daa3f1b955461314284
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2023-02-28 00:47:25 +0530

    fixed whatsapp issue and updated example with latest flutter version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,23 +20,23 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Colors.teal,
         body: ContactUs(
-            cardColor: Colors.white,
-            textColor: Colors.teal.shade900,
-            logo: AssetImage('images/logo.jpg'),
-            email: 'adoshi26.ad@gmail.com',
-            companyName: 'Abhishek Doshi',
-            companyColor: Colors.teal.shade100,
-            dividerThickness: 2,
-            phoneNumber: '+917818044311',
-            website: 'https://abhishekdoshi.godaddysites.com',
-            githubUserName: 'AbhishekDoshi26',
-            linkedinURL:
-                'https://www.linkedin.com/in/abhishek-doshi-520983199/',
-            tagLine: 'Flutter Developer',
-            taglineColor: Colors.teal.shade100,
-            twitterHandle: 'AbhishekDoshi26',
-            instagram: '_abhishek_doshi',
-            facebookHandle: '_abhishek_doshi'),
+          cardColor: Colors.white,
+          textColor: Colors.teal.shade900,
+          logo: const AssetImage('images/logo.jpg'),
+          email: 'adoshi26.ad@gmail.com',
+          companyName: 'Abhishek Doshi',
+          companyColor: Colors.teal.shade100,
+          dividerThickness: 2,
+          phoneNumber: '+917818044311',
+          website: 'https://abhishekdoshi.godaddysites.com',
+          githubUserName: 'AbhishekDoshi26',
+          linkedinURL: 'https://www.linkedin.com/in/abhishek-doshi-520983199/',
+          tagLine: 'Flutter Developer',
+          taglineColor: Colors.teal.shade100,
+          twitterHandle: 'AbhishekDoshi26',
+          instagram: '_abhishek_doshi',
+          facebookHandle: '_abhishek_doshi',
+        ),
       ),
     );
   }
