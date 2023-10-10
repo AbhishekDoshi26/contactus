f73aa8039b2842d63f9726385d9de6ac205c8880
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-10-10 09:46:42 +0530

    🎨 super key removed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
